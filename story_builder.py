58f01dfd9934727f2245d3967d8837bf6dfe9085
Simjrn <simonjenni374@gmail.com>
2025-10-05 15:15:13 +0100

    Create story_builder.py

---
+++ story_builder.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import time
+
+with st.sidebar:
+    page = st.radio("make or read", ["Make", 'Read'])
+
+if page == "Make":
+    with st.form("my_form"):
+        lang = st.text_input("What language is this for?")
+        name = '# ' + st.text_input("Enter the name of your story:")
+        story = st.text_area("Enter story:")
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        text = f"""{name}
+{story}"""
+        st.download_button(
+        label="Download Story",
+        data=text,
+        file_name=f"{lang}_{name.replace(' ', '_')}.txt",
+        mime="text/plain"
+    )
+elif page == "Read":
+    book = st.file_uploader("upload story", type='txt', accept_multiple_files=False)
+    container = st.container(border=True)
+    for line in book:
+        if line[0:13] == b'<writeAnswer>':
+            words = line[13:].decode('utf-8').split()
+            answer = st.text_input(f"What is: {words[0]}")
+            if answer == words[1]:
+                st.success("Well done!")
+        else:
+            st.markdown(line.decode('utf-8'))
+            time.sleep(2)
